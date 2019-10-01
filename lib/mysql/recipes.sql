fe2cfcdd6c3e34b9c4a4e10c301600a345ab1d60
JasonRenBang <54545506+JasonRenBang@users.noreply.github.com>
2019-10-01 14:25:48 -0400

    add database to recipes

---
+++ lib/mysql/recipes.sql
@@ -0,0 +1,10 @@
+-- ---------------------------
+-- recipes Insert
+-- Name: Zhichen Ren
+-- Course: CSCI 490
+-- Date: 10/1/2019
+-- Version: 1.0
+-- ----------------------------
+INSERT INTO recipes VALUES ( 0001, 'Fettuccine with Walnuts and parsley', 'Ellie Krieger creamy fettuccin will state your pasta craving without blowing');
+INSERT INTO recipes VALUES (0002, 'Baked Pork Chop', 'This simple preparation is perfect for a week night, just shake and bake');
+INSERT INTO recipes VALUES (0003, 'Shrimp and Grits', 'Give the Southern favorite a healthy makeover by scaling back on the ham and cream. With perfectly cooked shrimp atop warm grits, you will still have bliss in a bowl.');
