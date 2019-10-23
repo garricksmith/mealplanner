377a9131bb0e6a024fff2403906201ffa60e9835
garricksmith <garricksmith@gmail.com>
2019-10-23 11:20:18 -0400

    Started javafx portion of the project

---
+++ bin/src/MainMenu.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * MainMenu.java - Java class the implements main menu front-end
+ * 
+ * Author(s): Garrick Smith
+ * Last Edit: 10/21/2019
+*******************************************************************************/
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackFrame;
+import javafx.stage.Stage;
+
+public Class MainMenu extends Application{
+
+    Button button;
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Meal Planner");
+        
+        button = new Button();
+        button.setText("Click Me");
+        
+        StackPane layout = new StackPane();
+        layout.getChildren.add(button);
+        
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ bin/src/MealPlanner.java
@@ -0,0 +1,7 @@
+import javafx.application.Application;
+
+public class MealPlanner {
+    public static void main(String[] args) {
+        
+    }
+}
