1ab23fd48c3ea5af40941e78b952557031d6be9e
garricksmith <garricksmith@gmail.com>
2019-11-11 12:01:40 -0500

    Added login UI to javafx

--- bin/src/MainMenu.java
+++ bin/src/MainMenu.java
@@ -12,23 +12,64 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class MainMenu extends Application{
-
-    Button button;
-    
+public class MainMenu extends Application{  
     public static void main(String[] args) {
         launch(args);
     }
     
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Meal Planner");
+    public void signIn(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Meal Planner Login");
         
-        button = new Button();
-        button.setText("Click Me");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setCgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
+        
+        Text scenetitle = new Text("Meal Planner: Sign in");
+        scenetitle.setFont(Font.dont("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle,0,0,2,1);
+        
+        Label userName = new Label("User Name:");
+        grid.add(userName,0,1);
+        
+        TextField userTextField = new TextField();
+        grid.add(userTextField,1,1);
+        
+        Label pw = new Label("Password:");
+        grid.add(pwBox,1,2);
+        
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox,1,2);
         
         StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        layout.getChildren().add(grid);
+        
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn,1,4);
+        
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                Users u = new Users[1];
+                u = getUsersByUsername(userTextField.getText());
+                if(users[0] != null) {
+                    if(users[0].checkPassword(pwBox.getText());
+                        System.out.println("Login Successful");
+                    }
+                    else {
+                        System.out.println("Password does not match user");
+                    }
+                }
+                else {
+                    System.out.println("User does not exist");
+                }
+            }
+        }
         
         Scene scene = new Scene(layout, 300, 250);
         primaryStage.setScene(scene);
